ca09e2703f08a7d80191e4d8d03af48598bcd659
Mikhail Kravtsov <54990069+DbIXAHUEOKEAHA@users.noreply.github.com>
2022-12-02 19:29:32 +0800

    after zoom meeting

--- TC300.py
+++ TC300.py
@@ -9,16 +9,21 @@ rm = visa.ResourceManager()
 def get(device, command):
     '''device = rm.open_resource() where this function gets all devices initiaals such as adress, baud_rate, data_bits and so on; 
     command = string Standart Commands for Programmable Instruments (SCPI)'''
-    return device.query(command)
+    #return device.query(command)
+    return np.random.random(1)
 
 class TC300():
 
     def __init__(self, adress='ASRL3::INSTR'):
+        '''
         self.tc = rm.open_resource(adress, baud_rate=115200,
                                    data_bits=8, parity=constants.VI_ASRL_PAR_NONE,
                                    stop_bits=constants.VI_ASRL_STOP_ONE,
                                    flow_control=constants.VI_ASRL_FLOW_NONE,
                                    write_termination='\r', read_termination='\r')
+        
+        '''
+        self.tc = 0
 
         self.set_options = {'T1', 'T2'}
 
@@ -48,12 +53,12 @@ class TC300():
         self.tc.write('EN1=1')
         self.tc.write('TSET1=' + str(int(value * 10)))
 
-    def set_T1_from(self, t1_from=0):
+    def set_T1_MIN(self, t1_from=0):
         # Set the CH1 Target Temperature Min value,
         # (Range: -200 to TMAX1°C, with a resolution of 1°C).
         self.tc.write('TMIN1=' + str(t1_from))
 
-    def set_T1_to(self, t1_to=30):
+    def set_T1_MAX(self, t1_to=30):
         # Set the CH1 Target Temperature Max value, n equals value
         # TMIN1 to 400°C, with a resolution of 1°C).
         self.tc.write('T1MAX=' + str(t1_to))
@@ -73,12 +78,13 @@ class TC300():
         self.tc.write('EN2=1')
         self.tc.write('TSET2=' + str(int(value * 10)))
 
-    def set_T2_from(self, t2_from=0):
+    def set_T2_MIN(self, t2_from=0):
         # Set the CH2 Target Temperature Min value,
         # (Range: -200 to TMAX2°C, with a resolution of 1°C).
         self.tc.write('TMIN1=' + str(t2_from))
 
-    def set_T2_to(self, t2_to=20):
+    def set_T2_MAX(self, t2_to=20):
         # Set the CH2 Target Temperature Max value, n equals value
         # TMIN1 to 400°C, with a resolution of 1°C).
         self.tc.write('T1MAX=' + str(t2_to))
+    
